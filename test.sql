691d5a70cef86d6e263f616b12a9c96756b1cb71
tadink <tadink@proton.me>
2024-06-28 16:36:18 +0800

    Update test.sql

--- test.sql
+++ test.sql
@@ -9,8 +9,23 @@ create table if not exists actor
 
 create table if not exists actor_vod
 (
-    actor_id int null,
-    vod_id   int null
+    actor_id int default 0 not null,
+    vod_id   int default 0 not null
+);
+
+create table if not exists class
+(
+    id   int auto_increment
+        primary key,
+    name varchar(20) not null,
+    constraint tag_unique
+        unique (name)
+);
+
+create table if not exists class_vod
+(
+    class_id int default 0 not null,
+    vod_id   int default 0 not null
 );
 
 create table if not exists director
@@ -24,41 +39,30 @@ create table if not exists director
 
 create table if not exists director_vod
 (
-    director_id int null,
-    vod_id      int null
-);
-
-create table if not exists tag
-(
-    id   int auto_increment
-        primary key,
-    name varchar(20) not null,
-    constraint tag_unique
-        unique (name)
-);
-
-create table if not exists tag_vod
-(
-    tag_id int null,
-    vod_id int null
+    director_id int default 0 not null,
+    vod_id      int default 0 not null
 );
 
 create table if not exists vod
 (
-    id           int auto_increment
+    id          int auto_increment
         primary key,
-    name         varchar(255)              not null,
-    image        varchar(255)  default ''  not null,
-    remark       varchar(100)  default ''  not null,
-    rate         decimal(5, 1) default 0.0 not null,
-    alias        varchar(255)  default ''  not null,
-    area         varchar(30)   default ''  not null,
-    language     varchar(20)   default ''  not null,
-    publish_time int           default 0   not null,
-    update_time  datetime                  null,
-    source_url   varchar(255)  default ''  not null,
-    description  text                      null,
-    type_id      int           default 0   not null
+    type_id     int           default 0   not null,
+    name        varchar(255)              not null,
+    en_name     varchar(100)  default ''  not null,
+    sub         varchar(255)  default ''  not null,
+    status      tinyint       default 0   not null,
+    state       varchar(10)               null,
+    letter      varchar(10)   default ''  not null,
+    pic         varchar(255)  default ''  not null,
+    remark      varchar(100)  default ''  not null,
+    score       decimal(5, 1) default 0.0 not null,
+    area        varchar(30)   default ''  not null,
+    language    varchar(20)   default ''  not null,
+    year        int           default 0   not null,
+    vod_time    datetime                  null,
+    content     text                      null,
+    source_name varchar(10)   default ''  not null
 );
 
 create table if not exists vod_play_url
